a8c0139ad6aaee7a740dde113e71bd29d208c39f
Guillaume Richard <lord0gnome@gmail.com>
2018-11-14 19:57:37 +0100

    Update redux_async_middleware.js

--- dist/redux_async_middleware.js
+++ dist/redux_async_middleware.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 var asyncActionsMiddleware = function asyncActionsMiddleware(store) {
@@ -23,10 +21,12 @@ var asyncActionsMiddleware = function asyncActionsMiddleware(store) {
                 });
                 httpMethodToInvoke.apply(undefined, _toConsumableArray(params)).then(function (resultsObj) {
                     var successType = generateSuccessActionTypeName(type);
+                    console.log(resultsObj);
                     Promise.resolve(1).then(function () {
-                        return store.dispatch(_extends({
-                            type: successType
-                        }, resultsObj));
+                        return store.dispatch({
+                            type: successType,
+                            resultsObj: resultsObj
+                        });
                     });
                 }).catch(function (err) {
                     console.log(err);
